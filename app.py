13905edff37bc30334f2c6fa35ee52ca05d34e21
Jonathan <jonathanneo@me.com>
2021-06-07 15:10:18 +0800

    charts

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 # create flask app 
 app = Flask(__name__)
 
+# read in dataset
 df = pd.read_csv("retail.csv")
 
 @app.route("/")
@@ -21,6 +22,17 @@ def time_series(metric, years):
 def country(metric, years):
     if metric.lower() == "revenue":
         return  df[df["Year"].isin(map(int,years.split(",")))].groupby(by=["Retailer country"]).sum()["Revenue"].sort_values().to_dict()
+
+
+@app.route("/api/channel/<metric>/<years>")
+def channel(metric, years):
+    # Order method type
+    return df[df["Year"].isin(map(int,years.split(",")))].groupby(by=["Order method type"]).sum()["Revenue"].sort_values().to_dict()
+
+@app.route("/api/product/<metric>/<years>")
+def product(metric, years):
+    # Order method type
+    return df[df["Year"].isin(map(int,years.split(",")))].groupby(by=["Product"]).sum()["Revenue"].sort_values(ascending=False).head(10).to_dict()
 
 
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,16 @@
 var metric = "Revenue"
 var years = ["2004", "2005", "2006", "2007"]
 
+function sortObject(unsorted){
+    var sortedlist = Object.keys(unsorted).sort(function(a,b){return unsorted[a]-unsorted[b]});
+    var sorted = {}
+    sortedlist.forEach(function(item){
+        sorted[item]=unsorted[item]
+    })
+    return sorted;
+}
+
+
 $.get(`/api/timeseries/${metric.toLowerCase()}/${years}`, (res) => {
     var year = Object.keys(res);
     var metricValue = Object.values(res);
@@ -18,7 +28,7 @@ $.get(`/api/timeseries/${metric.toLowerCase()}/${years}`, (res) => {
         }
     ];
     var layout = {
-        title: `Time Series (${metric})`,
+        title: `${metric} Time Series`,
         yaxis: {
             title: `${metric} ($ Billions)`
         },
@@ -30,14 +40,6 @@ $.get(`/api/timeseries/${metric.toLowerCase()}/${years}`, (res) => {
     Plotly.newPlot('timeseries', data, layout);
 });
 
-function sortObject(unsorted){
-    var sortedlist = Object.keys(unsorted).sort(function(a,b){return unsorted[a]-unsorted[b]});
-    var sorted = {}
-    sortedlist.forEach(function(item){
-        sorted[item]=unsorted[item]
-    })
-    return sorted;
-}
 
 $.get(`/api/country/${metric.toLowerCase()}/${years}`, (res) => {
     
@@ -57,7 +59,7 @@ $.get(`/api/country/${metric.toLowerCase()}/${years}`, (res) => {
         }
     ];
     var layout = {
-        title: `Time Series (${metric})`,
+        title: `${metric} by Country`,
         xaxis: {
             title: `${metric} ($ Millions)`
         },
@@ -67,4 +69,64 @@ $.get(`/api/country/${metric.toLowerCase()}/${years}`, (res) => {
       };
     
     Plotly.newPlot('country', data, layout);
+});
+
+$.get(`/api/channel/${metric.toLowerCase()}/${years}`, (res) => {
+    
+    var resSorted = sortObject(res);
+    var channel = Object.keys(resSorted);
+    var metricValue = Object.values(resSorted);
+    var data = [
+        {
+            y: channel,
+            x: metricValue,
+            type: 'bar',
+            marker: {
+                color: '#212529',
+                width: 1,
+            },
+            orientation: 'h'
+        }
+    ];
+    var layout = {
+        title: `${metric} by Channel`,
+        xaxis: {
+            title: `${metric} ($ Billions)`
+        },
+        yaxis: {
+            title: `Channel`
+        }
+      };
+    
+    Plotly.newPlot('channel', data, layout);
+});
+
+$.get(`/api/product/${metric.toLowerCase()}/${years}`, (res) => {
+    
+    var resSorted = sortObject(res);
+    var product = Object.keys(resSorted);
+    var metricValue = Object.values(resSorted);
+    var data = [
+        {
+            y: product,
+            x: metricValue,
+            type: 'bar',
+            marker: {
+                color: '#212529',
+                width: 1,
+            },
+            orientation: 'h'
+        }
+    ];
+    var layout = {
+        title: `${metric} by Top 10 Products`,
+        xaxis: {
+            title: `${metric} ($ Millions)`
+        },
+        yaxis: {
+            title: `Product`
+        }
+      };
+    
+    Plotly.newPlot('product', data, layout);
 });
